c53e66253f31dea6400895308d05685c68dff950
JustSabda <dataerror13@gmail.com>
2021-10-08 16:39:19 +0700

    Bukti Belajar Part 10

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+    public static float coin = 300;
+    public static bool isGameover = false;
+    public static bool isComplate = false;
+}
---
+++ Assets/Scripts/TowerDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDestroy : MonoBehaviour
+{
+    public bool isPlayer = true;
+    private void OnDestroy()
+    {
+        if (!Data.isGameover)
+        {
+            if (isPlayer)
+            {
+                Data.isGameover = true;
+                Data.isComplate = false;
+                Debug.Log("Player Lost");
+            }
+            else
+            {
+                Data.isGameover = true;
+                Data.isComplate = true;
+                Debug.Log("Player Win");
+            }
+        }
+    }
+}
